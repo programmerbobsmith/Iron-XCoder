c1300227d82dca3a94b7d28a2ab941df7a0cede4
programmerbobsmith <algebrastudent23@gmail.com>
2022-10-09 16:37:47 -0500

    Add triangle functions

--- Iron_XCoder_Functions/cemath.c
+++ Iron_XCoder_Functions/cemath.c
@@ -2,13 +2,67 @@
 #include <stdint.h>
 #include <math.h>
 
+//finds the factorial of a number
 unsigned long long int fact(unsigned int num)
 {
 	unsigned int i;
 	unsigned long long int fact = 1;
+	//this multiplies `fact` by every number from 1 to `fact`
 	for(i = 1; i<=num; ++i)
 	{
 		fact*=i;
 	}
 	return (fact);
+}
+
+double pythag(double leg_1, double leg_2, double hyp)
+{
+	if (hyp == NULL)
+	{
+		return sqrt(leg_1*leg_1+leg_2*leg_2);
+	}else if (leg_2 == NULL)
+	{
+		return sqrt(hyp*hyp - leg_1*leg_1);
+	}else if (leg_1 == NULL)
+	{
+		return sqrt(hyp*hyp - leg_2*leg_2);
+	}
+}
+
+void solve_triangle(triangle_t triangle)
+{
+	switch(mode)
+	{
+		case 1: //side-angle-side
+			triangle.side_1 = sqrt(triangle.side_2*triangle.side_2+triangle.side_3*triangle.side_3-cos(pi/180*triangle.angle_1)*2*triangle.side_2*triangle.side_3);
+			triangle.angle_2 = 180/pi*acos((triangle.side_1*triangle.side_1+triangle.side_3*triangle.side_3-triangle.side_2*triangle.side_2)/(2*triangle.side_1*triangle.side_3));
+			triangle.angle_3 = 180/pi*acos((triangle.side_1*triangle.side_1+triangle.side_2*triangle.side_2-triangle.side_3*triangle.side_3)/(2*triangle.side_1*triangle.side_2));
+			triangle.perimeter = (triangle.side_a+triangle.side_b+triangle.side_c);
+			semip = triangle.perimeter/2;
+			triangle.area = sqrt(semip*(semip-triangle.side_a)*(semip-triangle.side_b)*(semip-triangle.side_c));
+		case 2: //side-side-side
+			triangle.angle_1 = 180/pi*acos((triangle.side_2*triangle.side_2+triangle.side_3*triangle.side_3-triangle.side_1*triangle.side_1)/(2*triangle.side_2*triangle.side_3));
+    		triangle.angle_2 = 180/pi*acos((triangle.side_1*triangle.side_1+triangle.side_3*triangle.side_3-triangle.side_2*triangle.side_2)/(2*triangle.side_1*triangle.side_3));
+			triangle.angle_3 = 180/pi*acos((triangle.side_1*triangle.side_1+triangle.side_2*triangle.side_2-triangle.side_3*triangle.side_3)/(2*triangle.side_1*triangle.side_2));
+    		triangle.perimeter = (side_a+side_b+side_c);
+			semip = triangle.perimeter/2;
+    		area = sqrt(semip*(semip-side_a)*(semip-side_b)*(semip-side_c));
+			break;
+		case 3: //angle-side-angle
+			triangle.angle_3 = 180 - triangle.angle_1 - triangle.angle_2;
+    		triangle.side_1 = (triangle.side_3*sin(pi/180*triangle.angle_1))/sin(pi/180*triangle.angle_3);
+    		triangle.side_2 = (triangle.side_1*sin(pi/180*triangle.angle_2))/sin(pi/180*triangle.angle_1);
+    		triangle.perimeter = (side_a+side_b+side_c);
+			semip = triangle.perimeter/2;
+    		triangle.area = sqrt(semip*(semip-triangle.side_1)*(semip-triangle.side_2)*(semip-triangle.side_3));
+			break;
+		case 4: //angle-angle-side
+			triangle.angle_3 = 180-triangle.angle_2-triangle.angle_3;
+    		triangle.side_2 = (triangle.side_1*sin(pi/180*triangle.angle_2))/sin(pi/180*triangle.angle_1);
+    		triangle.side_3 = (triangle.side_2*sin(pi/180*triangle.angle_3))/sin(pi/180*triangle.angle_2);
+    		triangle.perimeter = (triangle.side_a+triangle.side_b+triangle.side_c);
+			semip = triangle.perimeter/2;
+    		triangle.area = sqrt(semip*(semip-triangle.side_a)*(semip-triangle.side_b)*(semip-triangle.side_c));
+			break;
+	}
 }
--- Iron_XCoder_Functions/cemath.h
+++ Iron_XCoder_Functions/cemath.h
@@ -5,7 +5,21 @@
 extern "C" {
 #endif
 
-unsigned long long int fact(unsigned int num);
+extern global struct triangle
+{
+	double side_1;
+	double side_2;
+	double side_3;
+	double angle_1;
+	double angle_2;
+	double angle_3;
+	double perimeter;
+	double area;
+}
+#typedef triangle_t struct triangle
+unsigned long long int fact(unsigned int num)
+double pythag(double leg_1, double leg_2, double hyp)
+void solve_triangle(triangle_t triangle, uint8_t mode)
 
 #ifdef __cplusplus
 }
